be7af213ce53be94016f992106ee6f0105280424
Marc Bestmann <bestmann@informatik.uni-hamburg.de>
2020-12-16 15:56:26 +0100

    made move base optim work

--- src/parallel_parameter_search/move_base_optimization.py
+++ src/parallel_parameter_search/move_base_optimization.py
@@ -7,7 +7,9 @@ import roslaunch
 import rospkg
 import rospy
 import tf
+from actionlib_msgs.msg import GoalID
 from geometry_msgs.msg import PoseStamped, Twist
+from move_base_msgs.msg import MoveBaseActionGoal, MoveBaseActionResult
 from nav_msgs.msg import Odometry
 
 from parallel_parameter_search.abstract_ros_optimization import AbstractRosOptimization
@@ -39,29 +41,41 @@ class AbstractMoveBaseOptimization(AbstractRosOptimization):
         self.state_publisher.remap_args = [("/clock", "clock"), ("/tf", "tf"), ("/tf_static", "tf_static")]
         self.launch.launch(self.state_publisher)
         # we also need tf of base footprint
-        self.state_publisher = roslaunch.core.Node('humanoid_base_footprint', 'base_footprint',
-                                                   'base_footprint_publisher', namespace=self.namespace)
-        self.state_publisher.remap_args = [("/clock", "clock"), ("/tf", "tf"), ("/tf_static", "tf_static")]
-        self.launch.launch(self.state_publisher)
-        # start move base node
+        self.foot_print = roslaunch.core.Node('humanoid_base_footprint', 'base_footprint',
+                                              'base_footprint_publisher', namespace=self.namespace)
+        self.foot_print.remap_args = [("/clock", "clock"), ("/tf", "tf"), ("/tf_static", "tf_static")]
+        self.launch.launch(self.foot_print)
+        # we need odom fuser to provide odom tf
+        self.odom = roslaunch.core.Node('bitbots_odometry', 'odometry_fuser',
+                                        'odometry_fuser', namespace=self.namespace)
+        self.odom.remap_args = [("/clock", "clock"), ("/tf", "tf"), ("/tf_static", "tf_static"),
+                                ("motion_odometry", "true_odom")]
+        self.launch.launch(self.odom)
+        # start map server
         self.map_node = roslaunch.core.Node('map_server', 'map_server', 'map_server', namespace=self.namespace,
                                             args="$(find bitbots_localization)/models/field2019.yaml")
         self.map_node.remap_args = [("/clock", "clock"), ("/tf", "tf"), ("/tf_static", "tf_static")]
         self.launch.launch(self.map_node)
+        # fake localization
         self.tf_map_node = roslaunch.core.Node('bitbots_move_base', 'tf_map_odom.py', 'tf_odom_to_map',
                                                namespace=self.namespace)
         self.tf_map_node.remap_args = [("/clock", "clock"), ("/tf", "tf"), ("/tf_static", "tf_static")]
         self.launch.launch(self.tf_map_node)
+        # start move base node
         load_yaml_to_param(self.namespace + '/move_base/global_costmap', 'bitbots_move_base',
                            '/config/costmap_common_config.yaml', self.rospack)
         load_yaml_to_param(self.namespace + '/move_base/local_costmap', 'bitbots_move_base',
                            '/config/costmap_common_config.yaml', self.rospack)
         load_yaml_to_param(self.namespace + '/move_base', 'bitbots_move_base', '/config/local_costmap_config.yaml',
                            self.rospack)
-        load_yaml_to_param(self.namespace + '/move_base', 'bitbots_move_base', '/config/global_costmap_config.yaml', self.rospack)
-        load_yaml_to_param(self.namespace + '/move_base', 'bitbots_move_base', '/config/move_base_config.yaml', self.rospack)
-        load_yaml_to_param(self.namespace + '/move_base', 'bitbots_move_base', '/config/dwa_local_planner_config.yaml', self.rospack)
-        load_yaml_to_param(self.namespace + '/move_base', 'bitbots_move_base', '/config/global_planner_config.yaml', self.rospack)
+        load_yaml_to_param(self.namespace + '/move_base', 'bitbots_move_base', '/config/global_costmap_config.yaml',
+                           self.rospack)
+        load_yaml_to_param(self.namespace + '/move_base', 'bitbots_move_base', '/config/move_base_config.yaml',
+                           self.rospack)
+        load_yaml_to_param(self.namespace + '/move_base', 'bitbots_move_base', '/config/dwa_local_planner_config.yaml',
+                           self.rospack)
+        load_yaml_to_param(self.namespace + '/move_base', 'bitbots_move_base', '/config/global_planner_config.yaml',
+                           self.rospack)
         self.move_base_node = roslaunch.core.Node('move_base', 'move_base', 'move_base', namespace=self.namespace)
         self.move_base_node.remap_args = [("/clock", "clock"), ("/tf", "tf"), ("/tf_static", "tf_static")]
         self.launch.launch(self.move_base_node)
@@ -74,13 +88,27 @@ class AbstractMoveBaseOptimization(AbstractRosOptimization):
                                      ("/tf", "tf"), ("/tf_static", "tf_static")]
         self.launch.launch(self.walk_node)
 
-        # let nodes start
-        self.sim.run_simulation(100, 0.01)
+        # Let nodes start, otherwise the dynreconf client will timeout
+        while not rospy.is_shutdown():
+            try:
+                self.dynconf_client = dynamic_reconfigure.client.Client(self.namespace + '/move_base/DWAPlannerROS',
+                                                                        timeout=0)
+                break
+            except:
+                # wait some more till the service is up
+                self.sim.run_simulation(0.5, 0.1)
 
-        self.dynconf_client = dynamic_reconfigure.client.Client(self.namespace + '/move_base/DWAPlannerROS', timeout=60)
+        self.move_base_result = None
+        self.goal_publisher = rospy.Publisher(self.namespace + "/move_base/goal", MoveBaseActionGoal, tcp_nodelay=True,
+                                              queue_size=1)
+        self.cancel_publisher = rospy.Publisher(self.namespace + "/move_base/cancel", GoalID, tcp_nodelay=True,
+                                                queue_size=1)
+        self.move_base_subscriber = rospy.Subscriber(self.namespace + "/move_base/result", MoveBaseActionResult,
+                                                     self.result_cb,
+                                                     queue_size=1)
 
         self.number_of_iterations = 10
-        self.time_limit = 10
+        self.time_limit = 100
 
         # needs to be specified by subclasses
         self.reset_height_offset = None
@@ -92,7 +120,7 @@ class AbstractMoveBaseOptimization(AbstractRosOptimization):
                       create_goal_msg(0, 0, math.pi / 2),
                       create_goal_msg(2, 3, math.pi / 2)]
 
-    def objective(self, trial):
+    def objective(self, trial: optuna.Trial):
         # get parameter to evaluate from optuna
         self.suggest_params(trial)
         self.reset()
@@ -102,25 +130,49 @@ class AbstractMoveBaseOptimization(AbstractRosOptimization):
             d = 0
             for goal in self.goals:
                 d += 1
-                early_term, time_try = self.evaluate_direction(goal, trial)
+                early_term, time_try = self.evaluate_nav_goal(goal, trial)
                 # scale cost to time_limit
                 cost += time_try / self.time_limit
                 # check if we failed in this direction and terminate this trial early
                 if early_term:
                     # terminate early and give 1 cost for each try left
-                    return 1 * (self.number_of_iterations - iteration) * len(self.directions) + 1 * (
-                            len(self.directions) - d) + cost
+                    return 1 * (self.number_of_iterations - iteration) * len(self.goals) + 1 * (
+                            len(self.goals) - d) + cost
         return cost
 
-    def suggest_params(self, trial):
-        # todo
-        raise NotImplementedError
+    def suggest_params(self, trial: optuna.Trial):
+        param_dict = {}
 
-    def is_goal_reached(self):
-        # todo
-        raise NotImplementedError
+        def add(name, min_value, max_value):
+            param_dict[name] = trial.suggest_uniform(name, min_value, max_value)
 
-    def evaluate_nav_goal(self, goal: PoseStamped, trial: optuna.Trial):
+        def fix(name, value):
+            param_dict[name] = value
+            trial.set_user_attr(name, value)
+
+        add('max_vel_x', 0, 1)
+        add('max_vel_y', 0, 1)
+        add('max_vel_trans', 0, trial.params["max_vel_x"] + trial.params["max_vel_y"])
+        add('max_vel_theta', 0, 10)
+        fix('min_vel_x', - trial.params["max_vel_x"])
+        fix('min_vel_y', - trial.params["max_vel_y"])
+        fix('min_vel_trans', 0)
+        fix('min_vel_theta', 0)
+
+        add('acc_lim_x', 0, 5)
+        add('acc_lim_y', 0, 5)
+        add('acc_lim_trans', 0, 5)
+        add('acc_lim_theta', 0, 50)
+
+        add('path_distance_bias', 0, 10)
+        add('goal_distance_bias', 0, 10)
+        add('occdist_scale', 0, 10)
+        add('twirling_scale', 0, 10)
+
+        self.set_params(param_dict, self.dynconf_client)
+
+    def evaluate_nav_goal(self, goal: MoveBaseActionGoal, trial: optuna.Trial):
+        self.goal_publisher.publish(goal)
         start_time = self.sim.get_time()
         # wait till time for test is up or stopping condition has been reached
         while not rospy.is_shutdown():
@@ -129,7 +181,8 @@ class AbstractMoveBaseOptimization(AbstractRosOptimization):
             if passed_time > self.time_limit:
                 self.reset()
                 trial.set_user_attr('time_limit_reached',
-                                    (goal.pose.position.x, goal.pose.position.y, goal.pose.orientation))
+                                    (goal.goal.target_pose.pose.position.x, goal.goal.target_pose.pose.position.y,
+                                     goal.goal.target_pose.pose.orientation))
                 return True, self.time_limit
 
             # test if the robot has fallen down
@@ -140,7 +193,8 @@ class AbstractMoveBaseOptimization(AbstractRosOptimization):
                 # todo add only yaw instead of quaternion
                 # todo include max vel parameter or something similar
                 trial.set_user_attr('fallen',
-                                    (goal.pose.position.x, goal.pose.position.y, goal.pose.orientation))
+                                    (goal.goal.target_pose.pose.position.x, goal.goal.target_pose.pose.position.y,
+                                     goal.goal.target_pose.pose.orientation))
                 return True, self.time_limit
 
             # todo add test if the robot did not move for more than x seconds, to find oszillations more easily
@@ -159,6 +213,16 @@ class AbstractMoveBaseOptimization(AbstractRosOptimization):
         # was stopped before finishing
         raise optuna.exceptions.OptunaError()
 
+    def result_cb(self, msg: MoveBaseActionResult):
+        self.move_base_result = msg.status.status
+
+    def is_goal_reached(self):
+        # todo maybe check if it actually reached? but normally due to true_odom it should be fine
+        if self.move_base_result == 3:
+            self.move_base_result = None
+            return True
+        return False
+
     def reset_position(self):
         height = self.trunk_height + self.reset_height_offset
         pitch = self.trunk_pitch
@@ -169,9 +233,10 @@ class AbstractMoveBaseOptimization(AbstractRosOptimization):
         self.sim.reset_robot_pose((0, 0, height), (x, y, z, w))
 
     def reset(self):
+        # cancel goals and wait till robot stopped in walkready
+        self.cancel_publisher.publish(GoalID())
+        self.sim.run_simulation(1, 0.001)
         # reset simulation
-        # todo cancel goals, stop walking
-        # todo get in walkready
         self.reset_position()
 
     def set_cmd_vel(self, x, y, yaw):
@@ -184,17 +249,20 @@ class AbstractMoveBaseOptimization(AbstractRosOptimization):
 
 
 def create_goal_msg(x, y, yaw):
-    msg = PoseStamped()
-    msg.header.stamp = rospy.Time.now()
-    msg.header.frame_id = "map"
-    msg.pose.position.x = x
-    msg.pose.position.y = y
-    msg.pose.position.z = 0
+    time = rospy.Time.now()
+    msg = MoveBaseActionGoal()
+    msg.header.stamp = time
+    msg.header.frame_id = "base_foot_print"
+    msg.goal.target_pose.header.stamp = time
+    msg.goal.target_pose.header.frame_id = "map"
+    msg.goal.target_pose.pose.position.x = x
+    msg.goal.target_pose.pose.position.y = y
+    msg.goal.target_pose.pose.position.z = 0
     (x, y, z, w) = tf.transformations.quaternion_from_euler(0, 0, yaw)
-    msg.pose.orientation.x = x
-    msg.pose.orientation.y = y
-    msg.pose.orientation.z = z
-    msg.pose.orientation.w = w
+    msg.goal.target_pose.pose.orientation.x = x
+    msg.goal.target_pose.pose.orientation.y = y
+    msg.goal.target_pose.pose.orientation.z = z
+    msg.goal.target_pose.pose.orientation.w = w
 
     return msg
 
@@ -203,4 +271,6 @@ class WolfgangMoveBaseOptimization(AbstractMoveBaseOptimization):
     def __init__(self, namespace, gui, sim_type='pybullet'):
         super(WolfgangMoveBaseOptimization, self).__init__(namespace=namespace, gui=gui, robot_name='wolfgang',
                                                            sim_type=sim_type)
-        self.reset_height_offset = 0.005
+        self.reset_height_offset = 0.00
+        self.trunk_height = 0.4
+        self.trunk_pitch = 0.14
